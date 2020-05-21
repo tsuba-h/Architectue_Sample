9147a758d9562dc39ad922bc51519bc8bb88b407
Unknown <tsuba-h@arsaga.jp>
2020-05-22 01:48:27 +0900

    wip

---
+++ Architectue_Sample/MVVM/MVVMModel.swift
@@ -0,0 +1,72 @@
+//
+//  MVVMModel.swift
+//  MVVM
+//
+//  Created by 服部　翼 on 2020/05/22.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import Moya
+
+final class QiitaRepository {
+    private static let qiitaAPIProvider = MoyaProvider<Qiita>()
+    
+    static func jsonQiita(query: String,
+                          success: @escaping ([QiitaItem]) -> Void,
+                          failure: @escaping (Error) -> Void) {
+        
+        qiitaAPIProvider.request(.getQiita(query: query)) { (response) in
+            switch response {
+            case .success(let response):
+                do {
+                    let response = try JSONDecoder().decode([QiitaItem].self, from: response.data)
+                    success(response)
+                } catch (let error) {
+                    failure(error)
+                }
+            case .failure(let error):
+                failure(error)
+            }
+        }
+    }
+}
+
+struct  QiitaItem: Codable {
+    let title: String
+    let url: String
+}
+
+enum Qiita {
+    case getQiita(query: String)
+}
+
+extension Qiita: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://qiita.com")!
+    }
+    
+    var path: String {
+        return "/api/v2/items"
+    }
+    
+    var method: Method {
+        return .get
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .getQiita(let query):
+            return .requestParameters(parameters: ["per_page": "10", "query": query],
+                                      encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
+
---
+++ Architectue_Sample/MVVM/MVVMViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MVVMViewController.swift
+//  MVVM
+//
+//  Created by 服部　翼 on 2020/05/22.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import UIKit
+
+class MVVMViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MVVM")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
