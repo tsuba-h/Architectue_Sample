792a01371207ee3bb0c6a3ca5fbc8943ac8b19d3
服部翼 <tsuba-h@arsaga.jp>
2020-05-22 01:38:20 +0900

    Initial Commit

---
+++ Architectue_Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Architectue_Sample
+//
+//  Created by 服部　翼 on 2020/05/22.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
